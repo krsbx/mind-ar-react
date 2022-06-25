c7b45d40960678cd43b4bbae55c771059cfa7f79
KRSBX <firdaus.sati51@gmail.com>
2022-06-25 15:20:10 +0700

    chore: remove primitives

--- src/components/Assets/Items.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { IItems } from '../../utils/interfaces';
-
-const Items = ({ children, ...props }: IItems & { children?: React.ReactNode }) =>
-  React.createElement('a-asset-item', props, children);
-
-export default Items;
